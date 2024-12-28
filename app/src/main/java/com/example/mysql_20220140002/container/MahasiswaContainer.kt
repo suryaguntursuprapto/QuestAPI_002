6585f7c61cf84cd039e1603163eea6c5e526723c
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-12-28 21:21:41 +0700

    membuat MahasiswaContainer

---
+++ app/src/main/java/com/example/mysql_20220140002/container/MahasiswaContainer.kt
@@ -0,0 +1,32 @@
+package com.example.mysql_20220140002.container
+
+import com.example.mysql_20220140002.repository.MahasiswaRepository
+import com.example.mysql_20220140002.repository.NetworkKontakRepository
+import com.example.mysql_20220140002.service_api.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.create
+
+interface AppContainer{
+    val kontakRepository:MahasiswaRepository
+}
+
+class MahasiswaContainer:AppContainer{
+
+    private val baseUrl="http://10.0.2.2:8000/umyTI/" //LOCALHOST DIGANTI IP KALO RUN DI HP
+    private val json = Json { ignoreUnknownKeys=true }
+    private val retrofit:Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService:MahasiswaService by lazy{
+        retrofit.create(MahasiswaService::class.java)
+    }
+
+    override val kontakRepository: MahasiswaRepository by lazy {
+        NetworkKontakRepository(mahasiswaService)
+    }
+
+}
