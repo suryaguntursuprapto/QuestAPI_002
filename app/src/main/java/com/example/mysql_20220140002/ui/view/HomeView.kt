b31a8af8216afb692c92ce943ba11499aa1acfc3
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-12-29 11:44:40 +0700

    membuat object DestinasiHome pada HomeView

---
+++ app/src/main/java/com/example/mysql_20220140002/ui/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.example.mysql_20220140002.ui.view
+
+import com.example.mysql_20220140002.ui.navigation.DestinasiNavigasi
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
